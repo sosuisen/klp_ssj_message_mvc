fbed440ac5208a99e0ad8a87783cae4bf022dcf8
sosuisen <hidekazu.kubota@gmail.com>
2023-04-25 17:42:11 +0900

    fix: remove serialization

--- src/main/java/com/example/model/Messages.java
+++ src/main/java/com/example/model/Messages.java
@@ -1,17 +1,10 @@
 package com.example.model;
 
-import java.io.IOException;
 import java.io.Serializable;
-import java.nio.file.Files;
 import java.util.ArrayList;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
-import jakarta.json.bind.Jsonb;
-import jakarta.json.bind.JsonbBuilder;
-
 
 /**
  * MessagesはArrayListを継承するリストクラスです。
@@ -31,51 +24,4 @@ import jakarta.json.bind.JsonbBuilder;
 @ApplicationScoped
 @Named
 public class Messages extends ArrayList<MessageDTO> implements Serializable {
-	// データの保存先を適切なパスへ変更してください。
-	private String saveFilePath = "c:\\pleiades-ssj2023\\data.json";
-
-	/**
-	 * コンストラクタが呼ばれた直後に実行する処理。
-	 * 
-	 * @PostConstruct アノテーションを付けて定義します。
-	 * Messagesクラスはアプリケーションスコープであるため、
-	 * 生成されるのはアプリが起動したときの1度きりですから、
-	 * コンストラクタの中に処理を書いても結果は同じです。
-	 * 他のスコープだと動作は変わります。
-	 * 
-	 * saveFilePathからJSON文字列を読み込んだ後、
-	 * JsonbでMessagesオブジェクトへ変換し、リストへ追加しています。
-	 * addAllはArrayListから継承したメソッドなのでここで呼ぶことができます。
-	 */
-	@PostConstruct
-	public void prepare() {
-		Jsonb jsonb = JsonbBuilder.create();
-		try {
-			String json = Files.readString(java.nio.file.Path.of(saveFilePath));
-			addAll(jsonb.fromJson(json, Messages.class));
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
-	
-	/**
-	 * このインスタンスが破棄される直前に実行する処理。
-	 * 
-	 * @PreDestroy アノテーションを付けて定義します。
-	 * Messagesクラスはアプリケーションスコープであるため、
-	 * 破棄されるのはアプリが終了するときです。
-	 * 
-	 * リスト（this、つまり自分自身）の内容をJsonbでJSON文字列へ変換して、
-	 * saveFilePathへ保存しています。
-	 */
-	@PreDestroy
-	public void after() {
-		Jsonb jsonb = JsonbBuilder.create();
-		String json = jsonb.toJson(this);
-		try {
-			Files.writeString(java.nio.file.Path.of(saveFilePath), json);
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-	}
 }
